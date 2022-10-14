90b3aa09babdd38dace40fb91f2d33933b9e3fe7
rizky_abdi@ <65109996+rizki227@users.noreply.github.com>
2022-10-14 10:37:51 +0700

    Update variable, name function, coment, dan return

--- binary.js
+++ binary.js
@@ -1,7 +1,7 @@
-const data = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] // search value 2
-const name = prompt("Masukkan nama yang ingin dicari :")
+const data = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] 
+const NIM = prompt("Masukkan nama yang ingin dicari :")
 
-const binarySearchName = (target,data)=>{
+const binarySearchNim = (target,data)=>{
     
     let start = 0
     let end = data.length - 1
@@ -9,17 +9,18 @@ const binarySearchName = (target,data)=>{
         //Kita cari dulu nilai mid dalam array 
         let mid = Math.floor((start+end)/2)
         console.log(mid)
-        if(target ==  data[mid]) return console.log(`Hi ${target}, ID kamu tersedia `)
-        //jika target lebih kecil kita potong jangkauan dari akhir seacrh / end 
+        if(target ==  data[mid]) return alert(`NIM : ${target} kamu sudah terdaftar`)
+        //jika target lebih kecil kita potong jangkauan dari akhir seacrh / end yang niai end = mid - 1
         if(target < data[mid]){
             end = mid - 1
         }
+         //jika target lebih besar kita perluas jangkauan  seacrh / start yang niai startnya itu dimulai dari = mid - 1
         if(target > data[mid]){
             start = mid + 1
         }
     }
-    return console.log(`Maaf Id  ${target} tidak tersedia`)
+    return alert(`NIM : ${target} kamu belum terdaftar`)
 
 }
 
-binarySearchName(name,data)
+binarySearchNim(NIM,data)
